8628da172839034d875e004dba1a508682682dd2
akhil0072 <akhilso.rao@gmail.com>
2022-11-29 13:49:22 -0600

    added files html,css,script

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-<></>
+
+    function display(a){
+        console.log(a);
+        if(document.getElementById("myInput").value == '0'){
+            document.getElementById("myInput").value= "";
+        }
+        document.getElementById("myInput").value+=a;
+    }
+
+    function inputClear(){
+        document.getElementById("myInput").value = "0";
+    }
+
+    function check(){
+        var algebraicString = document.getElementById('myInput').value;
+        var result = [];
+        if(algebraicString.includes("+")){
+            result = algebraicString.split('+');
+            return(parseInt(result[0]) + parseInt(result[1]));
+        }
+        else if(algebraicString.includes("-")){
+            result = algebraicString.split('-');
+            return(parseInt(result[0]) - parseInt(result[1]));
+        }
+        else if(algebraicString.includes("/")){
+            result = algebraicString.split('/');
+            return(parseInt(result[0]) / parseInt(result[1]));
+        }
+        else{
+            result = algebraicString.split('*');
+            return(parseInt(result[0]) * parseInt(result[1]));
+        }
+    }
+    function result(){
+        var a = check();
+        document.getElementById('myInput').value = a;
+    }
