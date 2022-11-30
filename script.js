0f2d5860474cf79384f783136a747c8992d07536
dinesh17896 <dd17896@gmail.com>
2022-11-29 19:30:23 -0600

    updating the validation code for script

--- script.js
+++ script.js
@@ -16,22 +16,102 @@
         var result = [];
         if(algebraicString.includes("+")){
             result = algebraicString.split('+');
-            return(parseInt(result[0]) + parseInt(result[1]));
+		if(result[0].includes(".")){
+			var res = [];
+			res = result[0].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[1].includes(".")){
+			var res = [];
+			res = result[1].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[0].length >8 || result[1].length >8){
+		alert("The values entered must be less then length 8");
+		return 0;		
+		}
+            return(parseFloat(result[0]) + parseFloat(result[1]));
         }
         else if(algebraicString.includes("-")){
             result = algebraicString.split('-');
-            return(parseInt(result[0]) - parseInt(result[1]));
+		if(result[0].includes(".")){
+			var res = [];
+			res = result[0].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[1].includes(".")){
+			var res = [];
+			res = result[1].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[0].length >8 || result[1].length >8){
+		alert("The values entered must be less then length 8");
+		return 0;		
+		}
+            return(parseFloat(result[0]) - parseFloat(result[1]));
         }
         else if(algebraicString.includes("/")){
             result = algebraicString.split('/');
-            return(parseInt(result[0]) / parseInt(result[1]));
+		if(result[0].includes(".")){
+			var res = [];
+			res = result[0].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[1].includes(".")){
+			var res = [];
+			res = result[1].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[0].length >8 || result[1].length >8){
+		alert("The values entered must be less then length 8");
+		return 0;		
+		}
+            return(parseFloat(result[0]) / parseFloat(result[1]));
         }
         else{
             result = algebraicString.split('*');
-            return(parseInt(result[0]) * parseInt(result[1]));
+		if(result[0].includes(".")){
+			var res = [];
+			res = result[0].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[1].includes(".")){
+			var res = [];
+			res = result[1].split(".");
+			if(res[1].length >3){
+				alert("the decimal length should be less than 3");
+				return 0;
+			}
+		}
+		if(result[0].length >8 || result[1].length >8){
+		alert("The values entered must be less then length 8");
+		return 0;		
+		}
+            return(parseFloat(result[0]) * parseFloat(result[1]));
         }
     }
     function result(){
         var a = check();
-        document.getElementById('myInput').value = a;
+        document.getElementById('myInput').value = a;   
     }
